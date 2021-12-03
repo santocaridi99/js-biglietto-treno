89b485ae378210fb01cbafeabe3dc27011879ee2
santocaridi99 <santocaridi99@gmail.com>
2021-12-03 16:38:48 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
 //qui dico che outputHtml avrà accesso al documento tramite id ouput
 const outputHtml =  document.getElementById('output');
 //qui chiederò all'utente di inserire numero km che vuole percorrere con un prompt
-
+let numberKm = prompt('inserisci numero km che  vuoi percorrere');
+numberKm = parseInt(numberKm);
+console.log(numberKm);
 //qui chiederò all'utente l'età del passeggero attraverso prompt
-
+let age = prompt('inserisci età del passeggero');
+age = parseInt(age);
+console.log(age);
 //qui dichiaro prezzo biglietto che dovrà essere (0.21 € a km)
 //il prezzo è un numero reale
-
+//dichiaro una costante k per indicare 0.21 euro ,che moltiplicherò ai km
+const k = 0.21;
+price = k * numberKm;
+price = parseFloat(price);
+console.log(price);
 //qui controllo se età è minore di 18 applico sconto del 20%
 //se età >=65 allora applico sconto del 40%
 //il resto paga senza sconti :D
+//dichiaro prima i miei sconti
+let sconto1= (price * 20) / 100; 
+sconto1 = parseFloat(sconto1);
+let sconto2=(price * 40) / 100;
+sconto2 = parseFloat(sconto2);
+//dichiaro variabile final price
+let finalPrice;
+//qui svolgo la mia condizione
+if(age<18){
+    finalPrice = price - sconto1;
+    finalPrice=parseFloat(finalPrice).toFixed(2);
+    outputHtml.innerHTML = `Il tuo prezzo è : ${finalPrice}€`;
+}else if(age>=65){
+    finalPrice = price - sconto2;
+    finalPrice=parseFloat(finalPrice).toFixed(2);
+    outputHtml.innerHTML = `il tuo prezzo è: ${finalPrice}€`;
+}else{
+    finalPrice = price;
+    outputHtml.innerHTML = `il tuo prezzo è: ${finalPrice}€`;
+}
