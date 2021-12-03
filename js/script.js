f3e28a0af55a1ef441348d5ba1a0089981eb91d3
santocaridi99 <santocaridi99@gmail.com>
2021-12-03 15:22:59 +0100

    creazione file e aggiunta commenti per organizzare l'esercizio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//qui dico che outputHtml avrà accesso al documento tramite id ouput
+const outputHtml =  document.getElementById('output');
+//qui chiederò all'utente di inserire numero km che vuole percorrere con un prompt
+
+//qui chiederò all'utente l'età del passeggero attraverso prompt
+
+//qui dichiaro prezzo biglietto che dovrà essere (0.21 € a km)
+//il prezzo è un numero reale
+
+//qui controllo se età è minore di 18 applico sconto del 20%
+//se età >=65 allora applico sconto del 40%
+//il resto paga senza sconti :D
